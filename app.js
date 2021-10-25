f0afe91e49838670620c1d5a546a4ec3f6fb2e22
Henrique Silva <henriwasd@gmail.com>
2021-10-24 17:31:29 -0700

    Finalizando o bot de dor e sofrimento

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const twit = require('twit');
+require('dotenv/config');
+
+const app = express();
+
+const flutterBot = new twit({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+  timeout_ms: 60 * 1000,
+});
+
+const getTimeToFridayAt18 = () => {
+  const today = new Date();
+  const friday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5 - today.getDay());
+  const diff = friday - today;
+  const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  return { days, hours, minutes };
+};
+
+// flutterBot.post('statuses/update', { status: `Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} para sexta feira às 18hrs` }, (err, data, response) => {
+//   console.log(data);
+// }, (err, data, response) => {
+//   console.log('successfull');
+// }
+// );
+
+setInterval(() => {
+  const timeToFriday = getTimeToFridayAt18();
+  flutterBot.post('statuses/update', { status: `Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} para sexta feira às 18hrs` }, (err, data, response) => {
+    console.log(data);
+  }, (err, data, response) => {
+    console.log('successfull');
+  }
+  );
+}, 1000 * 60 * 60 * 8);
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+}
+);
+
+
+
