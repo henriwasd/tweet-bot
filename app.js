90a75dbab8656d4eeef69c393ddd565df7621cd7
Henrique Silva <henriwasd@gmail.com>
2021-10-28 09:08:14 -0300

    Melhorando o código do bot

--- app.js
+++ app.js
@@ -9,46 +9,27 @@ const flutterBot = new twit({
   timeout_ms: 60 * 1000,
 });
 
-const is8Hour = () => {
-  const date = new Date();
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-  return hour === 8 && minutes === 0;
-};
-
-const isWeekday = () => {
-  const date = new Date();
-  const day = date.getDay();
-  return day !== 0 && day !== 6;
-};
-
-const getTimeToFridayAt18 = () => {
+/// Returns the time to Friday at 18:00
+function getTimeToFridayAt18() {
   const today = new Date();
-  const friday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5 - today.getDay());
-  const diff = friday - today;
-  const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const friday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5 - today.getDay(), 18, 0, 0);
+  const timeToFriday = friday.getTime() - today.getTime();
+  return timeToFriday;
+}
+
+/// converter milisegundos para dias, horas e minutos
+function convertTime(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   return { days, hours, minutes };
-};
+}
+
 
 function postTweet() {
-  const timeToFriday = getTimeToFridayAt18();
-  flutterBot.post('statuses/update', { status: `Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} minutos para sexta feira às 18hrs` }, (err, data, response) => {
-    console.log(data);
-  }, (err, data, response) => {
-    console.log('successfull');
-  }
+  const timeToFriday = convertTime(getTimeToFridayAt18());
+  flutterBot.post('statuses/update', { status: `Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} minutos para sexta feira às 18 horas` }
   );
 }
 
-verifica = is8Hour() && isWeekday();
-while (true) {
-  if (verifica) {
-    postTweet();
-  }
-  verifica = false;
-  if (is8Hour()) {
-    verifica = is8Hour() && isWeekday();
-  }
-}
+postTweet();
