0f9ff28627a90df76558c367c82b4023db7476a7
henri <henriwasd@gmail.com>
2023-08-21 07:51:46 -0300

    Ajustado para o novo padrão de API do Twitter

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const twit = require('twit');
+const { TwitterApi } = require('twitter-api-v2');
 require("dotenv").config();
 
-const flutterBot = new twit({
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+const twitBot = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
   timeout_ms: 60 * 1000,
 });
 
 /// Returns the time to Friday at 18:00
 function getTimeToFridayAt18() {
   const today = new Date();
-  const friday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5 - today.getDay(), 18, 0, 0);
+  const friday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 5 - today.getDay(),
+    18,
+    0,
+    0
+  );
   const timeToFriday = friday.getTime() - today.getTime();
   return timeToFriday;
 }
@@ -25,11 +32,9 @@ function convertTime(time) {
   return { days, hours, minutes };
 }
 
-
-function postTweet() {
+async function postTweet() {
   const timeToFriday = convertTime(getTimeToFridayAt18());
-  flutterBot.post('statuses/update', { status: `Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} minutos para sexta feira às 18 horas` }
-  );
+  await twitBot.v2.tweet(`Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} minutos para sexta feira às 18 horas`);
 }
 
 postTweet();
