e69fbd2a0711172f114bb6fb6b7325d249c231ee
henri <henriwasd@gmail.com>
2024-01-16 15:34:49 -0300

    feature: Docker

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { TwitterApi } = require('twitter-api-v2');
-var CronJob = require('cron').CronJob;
-require("dotenv").config();
+const { TwitterApi } = require('twitter-api-v2')
+var CronJob = require('cron').CronJob
+require("dotenv").config()
 
 const twitBot = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
@@ -8,11 +8,11 @@ const twitBot = new TwitterApi({
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessSecret: process.env.TWITTER_ACCESS_SECRET,
   timeout_ms: 60 * 1000,
-});
+})
 
 /// Returns the time to Friday at 18:00
 function getTimeToFridayAt18() {
-  const today = new Date();
+  const today = new Date()
   const friday = new Date(
     today.getFullYear(),
     today.getMonth(),
@@ -20,23 +20,26 @@ function getTimeToFridayAt18() {
     18,
     0,
     0
-  );
-  const timeToFriday = friday.getTime() - today.getTime();
-  return timeToFriday;
+  )
+  const timeToFriday = friday.getTime() - today.getTime()
+  return timeToFriday
 }
 
 /// converter milisegundos para dias, horas e minutos
 function convertTime(time) {
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  return { days, hours, minutes };
+  const days = Math.floor(time / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+  return { days, hours, minutes }
 }
 
 async function postTweet() {
-  const timeToFriday = convertTime(getTimeToFridayAt18());
-  await twitBot.v2.tweet(`Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} minutos para sexta feira às 18 horas`);
-  console.log('Tweet posted!');
+  const timeToFriday = convertTime(getTimeToFridayAt18())
+  const bomDiaDorSofrimento = `Bom dia \nDor e Sofrimento \nFaltam ${timeToFriday.days} dias ${timeToFriday.hours} horas e ${timeToFriday.minutes} minutos para sexta feira às 18 horas`
+  await twitBot.v2.tweet(bomDiaDorSofrimento)
+  console.log('Tweet posted!')
+  console.log(bomDiaDorSofrimento)
+  console.log(estoyDeFerias)
 }
 
-new CronJob('0 8 * * 1-5', postTweet, null, true, 'America/Sao_Paulo');
+new CronJob('0 8 * * 1-5', postTweet, null, true, 'America/Sao_Paulo')
